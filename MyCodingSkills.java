169b7347e4ca32672248d88c93f74195f798ff17
scottbooma <scottbooma@gmail.com>
2022-05-25 12:03:07 -0400

    Add tech skills and print method

--- MyCodingSkills.java
+++ MyCodingSkills.java
@@ -1,5 +1,22 @@
 public class MyCodingSkills {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        MyCodingSkills.printMySkills(myTechStack);
+    }
+
+    private static String[] myTechStack = {
+        "git",
+        "CLI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TypeScript",
+        "Angular",
+        "Testing",
+    };
+
+    private static void printMySkills(String[] skillsArray){
+        for(String skill : skillsArray){
+            System.out.println(skill);
+        }
     }
 }
